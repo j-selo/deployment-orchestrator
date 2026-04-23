d7969848a5bde68e66f42e7e72b1514c853dc5bc
Jonathan Selormey <hello@jonathanselormey.com>
2026-04-23 16:35:51 -0400

    Modifying route

--- app/api/routes/deploy.py
+++ app/api/routes/deploy.py
@@ -3,16 +3,16 @@ from db import get_db
 from app.repositories.deploy_repo import DeployRepository
 from app.services.deploy_service import DeployService
 from temporalio.client import Client
-from app.schemas import DeployRequest
+from app.schemas import DeployRequest, DeployResponse
 
 router = APIRouter()
 
 # Creating a POST API endpoint for handling deployment requests
 @router.post("/")
 async def deploy(req: DeployRequest):
-    db        = await get_db()                      # Connect to the database
-    repo      = DeployRepository(db)                # Load repository with DB connection
+    db        = await get_db()                          # Connect to the database
+    repo      = DeployRepository(db)                    # Load repository with DB connection
     client    = await Client.connect("localhost:7233")  # Connect to Temporal client
-    service   = DeployService(repo, client)         # Load service with repository and Temporal client
-    deploy_id = await service.schedule(req)         # Schedule deployment, trigger the workflow and status updates
-    return {"message": f"Deployment request received {{'deploy_id': {deploy_id}}}"}
+    service   = DeployService(repo, client)             # Load service with repository and Temporal client
+    deploy_id = await service.schedule(req)             # Schedule deployment, trigger the workflow and status updates
+    return DeployResponse(id=deploy_id, status="scheduled")
